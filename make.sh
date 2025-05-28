21a8f827ed24a7999c5f7e5db46218d9a1683fff
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-05-28 19:11:39 +0200

    Version was hardcoded

--- make.sh
+++ make.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 
 version=$(yq -r .freelens .versions.yaml)
 
-releaseDate=$(gh release view v1.3.0 -R freelensapp/freelens --json publishedAt --jq .publishedAt | sed 's/T.*//')
-releaseUrl=$(gh release view v1.3.0 -R freelensapp/freelens --json url --jq .url)
-installerX64Url=$(gh release view v1.3.0 -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-amd64.exe").url')
-installerArm64Url=$(gh release view v1.3.0 -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-arm64.exe").url')
-installerX64Sha256Url=$(gh release view v1.3.0 -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-amd64.exe.sha256").url')
-installerArm64Sha256Url=$(gh release view v1.3.0 -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-arm64.exe.sha256").url')
+releaseDate=$(gh release view v"${version}" -R freelensapp/freelens --json publishedAt --jq .publishedAt | sed 's/T.*//')
+releaseUrl=$(gh release view v"${version}" -R freelensapp/freelens --json url --jq .url)
+installerX64Url=$(gh release view v"${version}" -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-amd64.exe").url')
+installerArm64Url=$(gh release view v"${version}" -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-arm64.exe").url')
+installerX64Sha256Url=$(gh release view v"${version}" -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-amd64.exe.sha256").url')
+installerArm64Sha256Url=$(gh release view v"${version}" -R freelensapp/freelens --json assets --jq '.assets[] | select(.name == "Freelens-'"${version}"'-windows-arm64.exe.sha256").url')
 installerX64Sha256=$(curl -sSL "${installerX64Sha256Url}" | cut -f1 -d' ' | tr '[:lower:]' '[:upper:]')
 installerArm64Sha256=$(curl -sSL "${installerArm64Sha256Url}" | cut -f1 -d' ' | tr '[:lower:]' '[:upper:]')
 
-gh release view v1.3.0 -R freelensapp/freelens --json body --jq .body | tr -d '\015' | sed -z 's/\n*$//' >release-notes.tmp
+gh release view v"${version}" -R freelensapp/freelens --json body --jq .body | tr -d '\015' | sed -z 's/\n*$//' >release-notes.tmp
 mkdir -p manifests/f/Freelensapp/Freelens/"${version}"
 
 yq '
